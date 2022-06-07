7f3184ef499fd258459da9d1f503e6a66b50e7ea
ClementPan <clementpan2022@gmail.com>
2022-06-07 15:30:51 +0800

    Feat: add cors npm

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const cors = require('cors')
 
 // require routes
 const routes = require('./routes')
 
 // require Todo
 const { urlencoded } = require('body-parser')
+
+
+app.use(cors())
 
 app.use(methodOverride('_method'))
 
