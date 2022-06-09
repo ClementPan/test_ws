363e3e8d909d721f571bd06e22640a91bad632d9
ClementPan <clementpan2022@gmail.com>
2022-06-09 16:13:05 +0800

    fix: change to websocket

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 // settings
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
+const PORT = process.env.PORT || 3000
 const cors = require('cors')
-
-// require routes
-const routes = require('./routes')
-
-// require Todo
-const { urlencoded } = require('body-parser')
-
+const SocketServer = require('ws').Server
+// const bodyParser = require('body-parser')
+// const methodOverride = require('method-override')
+// const { urlencoded } = require('body-parser')
+// const routes = require('./routes')
 
 app.use(cors())
+// app.use(methodOverride('_method'))
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(routes)
 
-app.use(methodOverride('_method'))
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+const wss = new SocketServer({ server })
 
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use(routes)
-
-app.listen(port, () => {
-  console.log(`The app is listening on http://localhost:${port}.`)
+wss.on('connection', ws => {
+  console.log('[[[ Client connected')
+  ws.on('message', m => {
+    console.log('[[[ on message: ', m.toString());
+  })
+  ws.on('close', () => {
+    console.log('[[[ Close connected')
+  })
 })
